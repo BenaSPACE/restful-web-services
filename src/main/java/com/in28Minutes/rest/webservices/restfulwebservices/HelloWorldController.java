fc90f89a4f7b28287ba5c0c2da675b9c28f7ce30
Benjamin McQueen <BenaSPACE@gmail.com>
2022-07-11 17:47:05 -0400

    7/11 end of study push

--- src/main/java/com/in28Minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28Minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -15,4 +15,9 @@ public class HelloWorldController {
         return "Hello World";
     }
 
+    //hello-world-bean
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
 }
