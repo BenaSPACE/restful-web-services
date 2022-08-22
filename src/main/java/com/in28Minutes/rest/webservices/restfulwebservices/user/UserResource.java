0ce35dc5e4f4cdfab715821a9971e5f7f5d5d4b4
Benjamin McQueen <BenaSPACE@gmail.com>
2022-08-22 19:44:10 -0400

    8/22 end of study push
    https://web.postman.co/workspaces

--- src/main/java/com/in28Minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28Minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,9 +1,7 @@
 package com.in28Minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,4 +25,13 @@ public class UserResource {
     }
 
     //retrieveUser(int id)
+
+    //CREATED
+    //input - details of user
+    //output - CREATED & Return the created URI
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+    }
 }
